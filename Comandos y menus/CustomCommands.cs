5f5e80c3e8356fb0a7e727c1649ce0b4aac5b1e0
Vicente Iglesias Montoya <vicente.iglesias@iesdoctorblamis.com>
2019-12-02 21:26:37 +0100

    Terminado

---
+++ Comandos y menus/CustomCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Comandos_y_menus
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Salir = new RoutedUICommand(
+
+            "Salir",
+            "Salir",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.S, ModifierKeys.Control)
+            }
+
+            );
+
+        public static readonly RoutedUICommand Vaciar = new RoutedUICommand(
+
+            "Vaciar",
+            "Vaciar",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.V, ModifierKeys.Alt)
+            }
+
+            );
+    }
+}
--- Comandos y menus/MainWindow.xaml.cs
+++ Comandos y menus/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace Comandos_y_menus
 {
@@ -20,9 +21,96 @@ namespace Comandos_y_menus
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ObservableCollection<TextBlock> lista;
+
+        TextBlock itemCopiado;
+
         public MainWindow()
         {
+            lista = new ObservableCollection<TextBlock>();
             InitializeComponent();
+            ListaListBox.DataContext = lista;
         }
+
+        private void CommandBinding_Executed_NUEVO(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBlock texto = new TextBlock();
+            texto.Text = "Item añadido a las " + DateTime.Now.ToLongTimeString();
+            lista.Add(texto);
+        }
+
+        private void CommandBinding_CanExecute_NUEVO(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(lista.Count < 10)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+        private void CommandBinding_Executed_SALIR(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CommandBinding_CanExecute_SALIR(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_VACIAR(object sender, ExecutedRoutedEventArgs e)
+        {
+            lista.Clear();
+        }
+
+        private void CommandBinding_CanExecute_VACIAR(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(lista.Count > 0)
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void CommandBinding_Executed_COPIAR(object sender, ExecutedRoutedEventArgs e)
+        {
+            itemCopiado = (TextBlock)ListaListBox.SelectedItem;
+        }
+
+        private void CommandBinding_CanExecute_COPIAR(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(lista.Count > 0 && ListaListBox.SelectedItem != null)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+        private void CommandBinding_Executed_PEGAR(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBlock textoACopiar = new TextBlock();
+            textoACopiar.Text = itemCopiado.Text;
+            lista.Add(textoACopiar);
+            itemCopiado = null;
+        }
+
+        private void CommandBinding_CanExecute_PEGAR(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if(itemCopiado != null && lista.Count < 10)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
     }
 }
